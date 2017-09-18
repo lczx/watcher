7ce2aeed648d7c298b14d203c96bc2098122daaa
Luca Zanussi <luca.z@outlook.com>
2017-09-18 22:16:05 +0200

    Use Toolbar in MainActivity

--- src/main/kotlin/net/hax/niatool/MainActivity.kt
+++ src/main/kotlin/net/hax/niatool/MainActivity.kt
@@ -2,12 +2,14 @@ package net.hax.niatool
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
     }
 
 }
