7705647600ef25d044e216a1f73498380a4fe92e
Luca Zanussi <luca.z@outlook.com>
2017-09-19 12:20:52 +0200

    Use a custom layout to dispatch touch events to non-square buttons

---
+++ src/main/kotlin/net/hax/niatool/widget/DispatcherLayout.kt
@@ -0,0 +1,117 @@
+package net.hax.niatool.widget
+
+import android.content.Context
+import android.graphics.Path
+import android.graphics.RectF
+import android.graphics.Region
+import android.support.annotation.AttrRes
+import android.support.annotation.StyleRes
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.FrameLayout
+import net.hax.niatool.R
+
+/**
+ * A [FrameLayout] that binds [Regions][Region] to its children; Touch events are dispatched to the first view whose
+ * region contains the touch point.
+ *
+ * Regions are defined by strings in a resource string array passed as the `dispatcherRegions` attribute. The index
+ * of the string in the array is the index of the bound view in the layout. The string follows the same format as
+ * SVG `d` paths or Android vector resources `pathData`.
+ */
+class DispatcherLayout(context: Context, attrs: AttributeSet?, @AttrRes defStyleAttr: Int, @StyleRes defStyleRes: Int)
+    : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
+            : this(context, attrs, defStyleAttr, 0)
+
+    constructor(context: Context, attrs: AttributeSet?)
+            : this(context, attrs, 0)
+
+    constructor(context: Context)
+            : this(context, null)
+
+    companion object {
+        private val TAG = "DispatcherLayout"
+        private val REFLECT_PATH_PARSER_CLASS = "android.util.PathParser"
+        private val REFLECT_PATH_PARSER_METHOD = "createPathFromPathData"
+        // We are using this instead of a styleable because there are no reasons to make this styleable
+        private val CUSTOM_ATTRS = intArrayOf(R.attr.dispatcherRegions)
+    }
+
+    private val mDensity = resources.displayMetrics.density
+    private val mRegions: List<Region>
+
+    init {
+        if (attrs == null)
+            throw IllegalArgumentException("DispatcherLayout requires a dispatcherRegions attribute to work")
+
+        val data = context.obtainStyledAttributes(attrs, CUSTOM_ATTRS, defStyleAttr, defStyleRes)
+        val paths = data.getTextArray(0) ?:
+                throw IllegalArgumentException("DispatcherLayout requires a dispatcherRegions attribute to work")
+        data.recycle()
+
+        mRegions = parsePaths(paths)
+    }
+
+    private fun parsePaths(paths: Array<out CharSequence>): List<Region> {
+        val createPathMethod = try {
+            Class.forName(REFLECT_PATH_PARSER_CLASS).getMethod(REFLECT_PATH_PARSER_METHOD, String::class.java)
+        } catch (e: ReflectiveOperationException) {
+            throw UnsupportedOperationException(
+                    "Could not find $REFLECT_PATH_PARSER_CLASS#$REFLECT_PATH_PARSER_METHOD(String) through reflection", e)
+        }
+
+        return paths.map {
+            val path = try {
+                createPathMethod.invoke(null, it) as Path
+            } catch (e: ReflectiveOperationException) {
+                throw UnsupportedOperationException("Reflection exception while parsing a path", e)
+            }
+
+            Region().apply {
+                val pb = RectF()
+                path.computeBounds(pb, true)
+                setPath(path, Region(pb.left.toInt(), pb.top.toInt(), pb.right.toInt(), pb.bottom.toInt()))
+            }
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val actionMasked = ev!!.action and MotionEvent.ACTION_MASK
+        val posX = (ev.x / mDensity).toInt()
+        val posY = (ev.y / mDensity).toInt()
+
+        // Find the first region containing the event point - if we have no region, return
+        val idx = mRegions.indexOfFirst { it.contains(posX, posY) }
+        if (idx == -1) {
+            if (actionMasked != MotionEvent.ACTION_MOVE) // Do not spam this too
+                Log.d(TAG, "No region covering touch area ($posX, $posY), ignoring")
+            return true
+        }
+
+        if (idx >= childCount) {
+            Log.w(TAG, "Region #$idx covers touch area ($posX, $posY), but no matching view exists")
+            return true
+        }
+
+        // Adjust event offsets for the child view and dispatch
+        val view = getChildAt(idx)
+        val offsetX = (scrollX - view.left).toFloat()
+        val offsetY = (scrollY - view.top).toFloat()
+        ev.offsetLocation(offsetX, offsetY)
+        view.dispatchTouchEvent(ev)
+        ev.offsetLocation(-offsetX, -offsetY)
+        return true
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        if (childCount > mRegions.size)
+            Log.w(TAG, "View count is greater than region count, exceeding views will always be ignored")
+        else if (childCount < mRegions.size)
+            Log.w(TAG, "View count is lower than region count, exceeding paths will always be ignored")
+    }
+
+}
