abfdb7a7e1379091259d1a810669658984116095
Luca Zanussi <luca.z@outlook.com>
2017-09-22 16:15:10 +0200

    Animate control panel mode change

--- src/main/kotlin/net/hax/niatool/overlay/ControlPanelOverlay.kt
+++ src/main/kotlin/net/hax/niatool/overlay/ControlPanelOverlay.kt
@@ -7,10 +7,20 @@ import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.TranslateAnimation
 import android.widget.FrameLayout
+import net.hax.niatool.AnimationAdapter
 import net.hax.niatool.R
 
 class ControlPanelOverlay(private val context: Context, private val commandListener: OnCommandListener? = null) {
+
+    companion object {
+        private val USE_ANIMATIONS = true
+        private val ANIMATION_DURATION_MS = 150L
+    }
 
     private val uiHandler = Handler(Looper.getMainLooper())
     private val viewCapture = createView(R.layout.overlay_ctrl_capture)
@@ -20,8 +30,26 @@ class ControlPanelOverlay(private val context: Context, private val commandListe
     private var inBrowseMode = false
         set(value) {
             if (field == value) return
-            viewport.removeAllViews()
-            viewport.addView(if (value) viewBrowse else viewCapture)
+            if (!USE_ANIMATIONS) {
+                viewport.removeAllViews()
+                viewport.addView(if (value) viewBrowse else viewCapture)
+            } else {
+                val anim = TranslateAnimation(0.0f, -viewport.width.toFloat(), 0.0f, 0.0f)
+                anim.interpolator = AccelerateInterpolator()
+                anim.duration = ANIMATION_DURATION_MS
+                anim.setAnimationListener(object : AnimationAdapter() {
+                    override fun onAnimationEnd(animation: Animation?) {
+                        viewport.removeAllViews()
+                        viewport.addView(if (value) viewBrowse else viewCapture)
+                        val anim2 = TranslateAnimation(-viewport.width.toFloat(), 0.0f, 0.0f, 0.0f)
+                        anim2.interpolator = DecelerateInterpolator()
+                        anim2.duration = ANIMATION_DURATION_MS
+                        viewport.getChildAt(0).startAnimation(anim2)
+                    }
+                })
+                viewport.getChildAt(0).startAnimation(anim)
+
+            }
             field = value
             commandListener?.onModeChanged(value)
         }
--- src/main/kotlin/net/hax/niatool/utils.kt
+++ src/main/kotlin/net/hax/niatool/utils.kt
@@ -4,6 +4,7 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.res.Resources
+import android.view.animation.Animation
 
 fun launchActivityFromOverlay(ctx: Context, cls: Class<*>, flags: Int = 0) {
     // To avoid long startup if invoked from launcher: https://stackoverflow.com/questions/5600084
@@ -23,4 +24,12 @@ fun calculateControlToastYOffset(ctx: Context): Int {
 fun obtainStatusBarHeight(resources: Resources): Int {
     val resId = resources.getIdentifier("status_bar_height", "dimen", "android")
     return if (resId > 0) resources.getDimensionPixelSize(resId) else 0
+}
+
+abstract class AnimationAdapter : Animation.AnimationListener {
+    override fun onAnimationStart(animation: Animation?) {}
+
+    override fun onAnimationEnd(animation: Animation?) {}
+
+    override fun onAnimationRepeat(animation: Animation?) {}
 }
