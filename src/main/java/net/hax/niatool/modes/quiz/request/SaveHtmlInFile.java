648cf110c75c5ea327bee1a4c548c458708ef80f
Marco <marco.prina01@universitadipavia.it>
2018-12-13 00:27:47 +0100

    modificata classe in modo tale che possa essere modificata successivamente per poter salvare altre cose

--- src/main/java/net/hax/niatool/modes/quiz/request/SaveHtmlInFile.java
+++ src/main/java/net/hax/niatool/modes/quiz/request/SaveHtmlInFile.java
@@ -2,30 +2,37 @@ package net.hax.niatool.modes.quiz.request;
 
 import android.content.Context;
 
-import java.io.File;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 class SaveHtmlInFile {
     private Context context;
-    private final String filename = "html.html";
-
-    //catella logs/data+ora+minuti+secondi/
+    private final String filenameHTML = "html.html";
+    private final String path;
 
     public SaveHtmlInFile(Context context) {
         this.context = context;
+        this.path="logs/"+timeToString()+"/";
     }
 
     void save(String html) {
 
         FileOutputStream outputStream;
-        
+
 
         try {
-            outputStream = context.openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream = context.openFileOutput(path+filenameHTML, Context.MODE_PRIVATE);
             outputStream.write(html.getBytes());
             outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String timeToString(){
+        SimpleDateFormat sdf1 = new SimpleDateFormat("MMdd_hhmmss", Locale.ITALIAN);
+        return sdf1.format(new Date());
     }
 }
