46cac59d17d2f47c6c5134dda5addbf10fa4d72d
Marco <marco.prina01@universitadipavia.it>
2018-12-12 14:15:41 +0100

    aggiunte congiunzioni

--- src/main/java/net/hax/niatool/modes/quiz/request/UselessWordRemover.java
+++ src/main/java/net/hax/niatool/modes/quiz/request/UselessWordRemover.java
@@ -3,12 +3,14 @@ package net.hax.niatool.modes.quiz.request;
 public class UselessWordRemover {
     private final String articoli[] = {"il", "i|", "lo","|o", "la","|a", "i","|", "gli", "le","|e", "l'","l","|", "un", "uno", "una", "un'", "del", "dello", "della", "delle", "degli"};
     private final String preposizioni[] = {"di", "a", "da", "in", "con", "su", "per", "tra", "fra"};
+    private final String congiunzioni [] = {"e", "o", "con", "ma"};
 
     protected String lookIn(String frase) {
         frase = frase.toLowerCase();
 
         frase = eliminate(frase, articoli);
         frase = eliminate(frase, preposizioni);
+        frase = eliminate(frase, congiunzioni);
 
         return frase;
     }
