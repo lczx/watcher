72f1a08924cd8308cc44c17d2626fe82e47c0338
Luca Zanussi <luca.z@outlook.com>
2018-12-14 21:12:40 +0100

    Dump HTML file in logs directory

--- src/main/java/net/hax/niatool/modes/quiz/request/SaveHtmlInFile.java
+++ src/main/java/net/hax/niatool/modes/quiz/request/SaveHtmlInFile.java
@@ -2,25 +2,27 @@ package net.hax.niatool.modes.quiz.request;
 
 import android.content.Context;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class SaveHtmlInFile {
-    private Context context;
-    private final String filenameHTML = "html.html";
-    private final String path;
+
+    private static final String FILENAME_HTML = "html.html";
+    private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("MMdd_hhmmss", Locale.ROOT);
+
+    private final File logDir;
 
     public SaveHtmlInFile(Context context) {
-        this.context = context;
-        this.path="logs\\"+timeToString()+"\\";
+        logDir = new File(context.getExternalFilesDir(null), "logs/" + TIMESTAMP_FORMAT.format(new Date()));
+        logDir.mkdirs();
     }
 
     void save(String html) {
-        FileOutputStream outputStream;
         try {
-            outputStream = context.openFileOutput(path+filenameHTML, Context.MODE_APPEND);
+            FileOutputStream outputStream = new FileOutputStream(new File(logDir, FILENAME_HTML));
             outputStream.write(html.getBytes());
             outputStream.close();
         } catch (Exception e) {
@@ -28,8 +30,4 @@ public class SaveHtmlInFile {
         }
     }
 
-    private String timeToString(){
-        SimpleDateFormat sdf1 = new SimpleDateFormat("MMdd_hhmmss", Locale.ITALIAN);
-        return sdf1.format(new Date());
-    }
 }
